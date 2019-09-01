f7ffd8c882511333515db7622452c00c4a78010d
andrewaage <andre@rivenaes.net>
2019-09-01 19:37:25 +0200

    vignettes and data

---
+++ R/credit_df.R
@@ -0,0 +1,17 @@
+#' German credit data.
+#'
+#' A dataset containing credit data.
+#'
+#' @format A data frame with 1000 rows and 7 variables:
+#' \describe{
+#'   \item{Creditworthy}{indicator, is the customer creditworthy?}
+#'   \item{Duration_months}{months of credit}
+#'   \item{Credit_amount}{amount of credit}
+#'   \item{Age}{age of customer}
+#'   \item{Occupation}{occupation of customer}
+#'   \item{Foreign_worker}{= 2 if foreign, 1 else}
+#'   \item{Gender_marital}{Gender and marital status}
+#'   ...
+#' }
+#' @source \url{"http://freakonometrics.free.fr/german_credit.csv"}
+"credit_df"
